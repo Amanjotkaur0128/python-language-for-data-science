5351f77934687af9aa4677ee5a4e0d30bb7616cd
Amanjotkaur0128 <amankaurj0128@gmail.com>
2026-06-13 11:38:19 +0530

    strings in python language

---
+++ string.py
@@ -0,0 +1,57 @@
+# #string  - set of characters, numbers and some special characters
+
+# naam="Manjot Singh"
+# print(naam)
+# print(len(naam))
+# print(naam[1])
+
+
+# # methods 
+# print(naam.lower())
+# print(naam.upper())
+# print(naam.capitalize())
+# print(naam.title())
+# print(naam.swapcase())
+# print(naam.endswith('ngh')) # True or False answer
+# print(naam.startswith('Ma')) # True or False answer
+
+# print("----------------")
+# # replace in string
+# n="jeet"
+# print(n)
+# print(n.replace('jeet','i'))
+
+
+# # Slicing 
+# print(naam)
+# print(naam[:]) # start:stop:step
+# print(naam[:4]) # start:stop:step
+# print(naam[1:15]) # start:stop:step
+# print(naam[2:6]) # start:stop:step
+# print(naam[2:6:1]) # start:stop:step
+# print(naam[::-1]) # start:stop:step
+# print(naam[10:1:-2]) 
+
+
+
+# s="Hello how are you"
+# print(s)
+
+#split
+# print(s.split(','))
+# print(s.split(' '))
+
+# s1="hewevwe#dfgbd#bfdbdf#jergejn#kjgbfgjj#kjdbjk"
+# print(s1.split('#'))
+
+# e="    hello     "
+# print(e.strip())
+# print(e.lstrip())
+# print(e.rstrip())
+
+
+
+# the s is given to i in the loop and it will print the i with _ after it.
+#for examle in loop "for
+# for i in s:
+#     print(i+"_")
