19abd9b265a43d4203f4e6e2264b030ca21abb2e
Amanjotkaur0128 <amankaurj0128@gmail.com>
2026-06-13 11:35:49 +0530

    conditions in python language

---
+++ condition.py
@@ -0,0 +1,39 @@
+
+#conditional statements
+# control the flow of the program based on certain conditions
+# if, elif, else
+# nested if statements
+# c is block defined language and python is indentation based language
+# age=18
+# if age>=18:
+#     print("you are eligible to vote")
+#else:
+#     print("you are not eligible to vote")
+
+#a =10
+# print("the given no is",a)
+#print(f"the given no is {a}")
+
+
+# a=input("enter a age: ")
+# a=int(a)
+# if a>18:
+#     print("you are minor")
+#elif a==18:
+#     print("you are exactly 18")
+#else:
+#     print("you are old age person")
+
+
+#to check whether the given no is even or odd
+# a=int(input("enter a no: "))
+# if a%2==0 and a>0:
+#     print("the given no is even")
+# elif a%2!=0 and a>0:
+#     print("the given no is odd")
+# else:
+#      if a%2==0:
+#           print("the given no is negative even")
+#      else:
+#         print("the given no is negative odd")
+
