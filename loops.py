9332b76e1fdc0e5b521d6b3471e3ea00dd0e85f7
Amanjotkaur0128 <amankaurj0128@gmail.com>
2026-06-13 11:15:23 +0530

    loops in python

---
+++ loops.py
@@ -0,0 +1,40 @@
+#for loop
+
+#simple for loop
+# for i in range(11):
+#     print(i)
+ 
+# the starting point of the range can be defined by user "syntax: range(start, stop)"    
+# for i in range(1, 11):
+#     print(i)
+
+#to print odd no using step in range "syntax: range(start, stop, step)"
+# for i in range(1, 11, 2):
+#     print(i)
+
+#to print the reverse no "the minus work as 1-(-1) = 2" using this you can print the reverse no in negitive step.
+# for i in range(10, 0, -1):
+#     print(i)
+
+# to print the star pattern
+#for i in range(0,5):
+#    for j in range(0, i+1):
+#        print("*", end="")
+#    print()
+
+#while loop
+# a = 0
+# while a < 5:
+#     print(a)
+#     a += 1
+
+# to enter a name and print it the time of the length of the name
+# name = input("enter your name: ")
+# for i in range(len(name)):
+#     print(name)
+
+# methods of string
+
+# print(name.upper()) # to convert the string in upper case
+# print(name.lower()) # to convert the string in lower case
+# print(name.title()) # to convert the first letter of each word in upper case
