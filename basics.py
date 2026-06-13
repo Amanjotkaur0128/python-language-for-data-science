e7a9bf28b7a30ac826ec65e5d0732eae228f8d5b
Amanjotkaur0128 <amankaurj0128@gmail.com>
2026-06-13 11:12:25 +0530

    basic of python

---
+++ basics.py
@@ -0,0 +1,130 @@
+#  # data types, variables, and input/output
+# print("Hello, World!") 
+#  # this is a command to print something to the console
+# print("my name is aman",end=" ") 
+#  # end is a parameter that specifies what to print at the end of the output
+# print("and I am a student of cse") 
+# # this will be printed on the same line as the previous print statement
+
+#variables
+#int, float, string, boolean
+# a=10 
+# this is an integer data type
+# b=3.14
+#  this is a float data type
+# # c="Hello" 
+# this is a string data type
+# d=True 
+#  this is a boolean data type
+
+
+# print("the value of a is:",a" and the value of b is:",b) this will print the value of  a and b
+# print(type(a))
+#  # this will print the data type of a
+# print(a)
+
+# d= True
+# print(type(d))
+# print(d)
+ 
+# type conversion
+# i=10
+# print(float(i))
+# # this will convert the valuwe of i to a float data type
+# print (int(True))
+
+#opeators
+# operator and oprand
+# operator is a symbol that performs a specific operation on one or more operands
+# operand is a value that an operator operates onl 
+# a+b
+# arithmethic, assignment, comparison, logical, bitwise, identity, membership
+
+#user input
+#INPUT Reads the input in string format
+# x= int(input("enter a number: ")) 
+# # this will take input from the user and store it in the variable x
+# y=int(input("enter another number: ")) 
+# # this will take input from the user and store it in the variable y
+# print(x+y) 
+# # this will print the value of x
+
+#arithmetic operators
+# print(10+5)
+#  # this will print the sum of 10 and 5
+# print(10-5)
+#  # this will print the difference of 10 and 5
+# print(10*5)
+#  # this will print the product of 10 and 5
+# print(10/5) 
+# # this will print the quotient of 10 and 5    
+# print(a%b)  
+# print(a**b)  
+# print(a//b)#  # this will print the floor division of a and b
+ 
+#assignment operators : change the original value of the variables
+# a=10 
+# # this is an assignment operator that assigns the value 10 to the variable a
+# a+=5
+#  # this is an assignment operator that adds 5 to the value of a
+# a-=5 
+# # this is an assignment operator that subtracts 5 from the value of a
+# a*=5
+#  # this is an assignment operator that multiplies the value of a by 5
+# #division give the result in float
+# a/=5
+#  # this is an assignment operator that divides the value of a by 5
+
+#logical operators
+# and, or, not
+# a=True
+# b=False
+# print(a and b)# or, and is binary operator that returns True if both operands are true, otherwise it returns False
+# print(a or b)
+# print(not a)# not is a unary operator that returns True if the operand is false, otherwise it returns False
+
+#relation operators
+# ==, !=, >, <, >=, <=
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# bitwise operators
+# & bitwise and, | bitwise or, ^ bitwise xor, ~ bitwise not, << left shift, >> right shift
+# a=10         
+# b=5
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+# print(~a)
+# print(a << 2)
+# print(a >> 2)
+
+#ternary opearator
+#a= 10
+# b=20
+# print("a is greater than b") if a>b else print("b is greater than a")
+
+
+# membership operators
+# in, not in
+# a=1[1,2,3,4,5]
+
+#conditional statements
+# control the flow of the program based on certain conditions
+# if, elif, else
+# nested if statements
+# c is block defined language and python is indentation based language
+# age=18
+# if age>=18:
+#     print("you are eligible to vote")
+#else:
+#     print("you are not eligible to vote")
+
+#a =10
+
