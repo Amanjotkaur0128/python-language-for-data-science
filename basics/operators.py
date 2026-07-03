54ac364249c3b2c824dbdd914279f6a4c07cfd72
Amanjotkaur0128 <amankaurj0128@gmail.com>
2026-07-03 21:07:49 +0530

    operators in python

---
+++ basics/operators.py
@@ -0,0 +1,102 @@
+# Operators in python
+'''
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Bitwise Operators
+6. Membership Operators
+7. Ternary Operators
+'''
+
+# # Arithmetic Operators
+# a = 10 
+# b=6
+
+# print(a+b) # Addition
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+
+# # Logical Operators
+
+# x= True
+# y =False
+# # operands
+# # AND and OR are binary operators
+# # NOT is a unary operator
+# print(x and y)
+# print(x or y)
+# print(not x)
+# print(not y)
+
+
+# Relational / Comparison Operators
+# p=5
+# q=10
+
+# print(p==q)
+# print(p!=q)
+# print(p>=q)
+# print(p<=q)
+# print(p>q)
+# print(p<q)
+
+
+# Assignment Operators
+
+'''
+=, +=,-=,*=,/=,//=,**=
+a=a+1
+a+=1
+'''
+
+# a=10
+# a+=5
+# print(a)
+# a-=5
+# print(a)
+# a*=5
+# print(a)
+
+
+# Bitwise Operators
+
+'''
+& (AND)
+| (OR)
+^ (XOR)
+~ (NOT)
+<< (Left Shift)
+>> (Right Shift)
+'''
+
+# a=10
+# b=6
+
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+# print(~a)       
+# print(a << 1)   # Left Shift
+# print(a >> 1)   # Right Shift
+
+
+# Ternary Operators
+
+# a=10
+# b=20
+# # syntax: value_if_true if condition else value_if_false
+# result = "a is greater than b" if a > b else "b is greater than a"
+# print(result)
+
+# # Membership Operators
+# a = [1, 2, 3, 4, 5]
+# print(3 in a)
+# print(6 not in a)
+# print(2 not in a)
+# print(7 in a)
